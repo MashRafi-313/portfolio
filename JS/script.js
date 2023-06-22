8116ed48c1b676c5f5ef67b92fd30437be30db42
Mohammad Tusher Imran Mashrafi <122280570+MashRafi-313@users.noreply.github.com>
2023-06-23 02:16:02 +0600

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
-
+//typing animation   
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Android App Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
